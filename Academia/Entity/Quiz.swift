7bec65a60bcc37daa0fc9bf24d7c047376a69d43
dave-testaverde <davidetestaverde@gmail.com>
2025-01-24 19:16:32 +0100

    Added Quiz entity

---
+++ Academia/Entity/Quiz.swift
@@ -0,0 +1,18 @@
+//
+//  Quiz.swift
+//  Academia
+//
+//  Created by Dave on 24/01/25.
+//
+
+class Quiz {
+    private let question: String
+    private let answer: [String]
+    private let correct: Int
+    
+    init(question: String, answer: [String], correct: Int) {
+        self.question = question
+        self.answer = answer
+        self.correct = correct
+    }
+}
