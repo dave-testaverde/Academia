0af2aea14ce95ae334b24dcccfdb9b3f181c9796
dave-testaverde <davidetestaverde@gmail.com>
2025-02-06 18:21:44 +0100

    Added data-model embedding

---
+++ Academia/Shared/LLM/Embedding/Embedding.swift
@@ -0,0 +1,19 @@
+//
+//  Embedding.swift
+//  Academia
+//
+//  Created by Dave on 06/02/25.
+//
+
+struct Embedding: Decodable {
+    let model: String
+    var embeddings: [[Double]]
+    var total_duration: Int
+    var load_duration: Int
+    var prompt_eval_count: Int
+}
+
+struct EmbeddingRequest: Encodable {
+    let model: String
+    let input: String
+}
